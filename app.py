b9edb911f25f55afb08e8e14f4cc1182180cf488
marouan19 <marwandaghmoumi2017@gmail.com>
2024-06-13 21:54:48 +0100

    finish My Skills partie

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/single')
-def single():
-    return render_template('single.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
